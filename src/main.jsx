0472a960a88379f756c6a2c3807c1bb8f75381b6
Matthew Keys <mattkeys1991@gmail.com>
2026-03-26 15:37:51 -0500

    added React Router and activityDetails.jsx page

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,31 @@
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.jsx";
+// import Layout from "./layout/Layout";
+
+// import { AuthProvider } from "./auth/AuthContext";
+// import { BrowserRouter } from "react-router";
+
+// createRoot(document.getElementById("root")).render(
+//   <BrowserRouter>
+//     <AuthProvider>
+//       <Layout>
+//         <App />
+//       </Layout>
+//     </AuthProvider>
+//   </BrowserRouter>,
+// );
+
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Layout from "./layout/Layout";
-
 import { AuthProvider } from "./auth/AuthContext";
-import { PageProvider } from "./layout/PageContext";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <PageProvider>
-      <Layout>
-        <App />
-      </Layout>
-    </PageProvider>
-  </AuthProvider>,
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>,
 );
